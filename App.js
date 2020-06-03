1b1b3b5dc1e3381ee2cc1b1d53def29730798cea
Natalie Boardway <ngboardway@gmail.com>
2020-06-03 08:25:06 -0400

    Start with prof's hw-3 solution

--- App.js
+++ App.js
@@ -1,23 +1,38 @@
 import React from 'react';
 import { StyleSheet, SafeAreaView, Keyboard, TouchableWithoutFeedback } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import CalculatorScreen from './screens/CalculatorScreen';
+import SettingsScreen from './screens/SettingsScreen';
 
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-      <SafeAreaView style={styles.container} >
-        <CalculatorScreen />
-      </SafeAreaView>
-    </TouchableWithoutFeedback>
+    
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={navStyling}>
+          <Stack.Screen name="Geo Calculator" component={CalculatorScreen} />
+          <Stack.Screen name="Settings" component={SettingsScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
 
+const navStyling = {
+  headerStyle: {
+    backgroundColor: '#0065A4',
+  },
+  headerTintColor: '#fff',
+  headerTitleStyle: {
+    fontWeight: 'bold',
+  },
+};
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
     margin: 20,
-    flex: 1
+    flex: 1,
   },
 });
